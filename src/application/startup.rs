773121fd7ac7509066919fc67213eef13198e8f1
Igor Gilmutdinov <bladoff@gmail.com>
2021-06-11 15:43:24 +0300

    fixed app graceful shutdown

--- src/application/startup.rs
+++ src/application/startup.rs
@@ -1,4 +1,4 @@
-use crate::{actors::stash_receiver::StartReceiveMsg, application::configuration::Settings};
+use crate::application::configuration::Settings;
 use crate::implementations::http_controller::{calculate_pob, get_build_price};
 use crate::{
     actors::stash_receiver::StashReceiverActor,
@@ -11,8 +11,9 @@ use actix::prelude::*;
 use actix_web::{dev::Server, web, App, HttpServer};
 use diesel::{Connection, SqliteConnection};
 use jsonrpc_v2::Server as JsonrpcServer;
+use log::error;
 use std::net::TcpListener;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Receiver};
 use std::sync::{Arc, Mutex};
 use std::{thread, thread::JoinHandle};
 use tokio::sync::Mutex as AsyncMutex;
@@ -22,6 +23,7 @@ const USER_AGENT: &str = "OAuth poe-system/0.0.1 (contact: bladoff@gmail.com)";
 pub struct Application {
     server: Server,
     handle: JoinHandle<Result<(), std::io::Error>>,
+    rx: Receiver<System>,
 }
 
 embed_migrations!("migrations");
@@ -55,14 +57,16 @@ impl Application {
         let repo = Arc::new(Mutex::new(repo));
         let client = Arc::new(AsyncMutex::new(Client::new(USER_AGENT.to_owned())));
 
-        let (tx, rx) = channel::<Arc<actix::SystemRunner>>();
+        let (tx, rx) = channel::<actix::System>();
 
-        // идея для корректного отключения состоит в использовании арбитра (можно цепануть у системы)
-        // вероятно можно даже не создавать тред, но это не точно
         let handle = thread::spawn(move || {
-            let system = System::new();
+            let system_runner = System::new();
 
-            system.block_on(async {
+            let system = System::try_current().expect("cant get thread system");
+
+            tx.send(system).expect("cant send running system");
+
+            system_runner.block_on(async {
                 let actor = StashReceiverActor::new(repo.clone(), client.clone());
 
                 let actor = actor.start();
@@ -77,33 +81,53 @@ impl Application {
                 };
                 let _ = timer.start();
             });
-            system.run()
+            system_runner.run()
         });
 
-        Ok(Self { server, handle })
+        Ok(Self { server, handle, rx })
     }
 
     pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
         let result = self.server.await;
-        // TODO: somehow chain with result below
-        let result2 = self.handle.join();
-        result
+        let system = self.rx.recv().expect("cant get system from child thread");
+        system.stop();
+        let result2 = match self.handle.join() {
+            Ok(k) => k,
+            Err(e) => {
+                error!("child thread panicked: {:?}", e);
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "child thread panicked",
+                ))
+            }
+        };
+        result.and(result2)
     }
 }
 
 fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let rpc = JsonrpcServer::new()
         .with_method("calculate_pob", calculate_pob)
+        .finish();
+
+    let rpc_get = JsonrpcServer::new()
         .with_method("get_build_price", get_build_price)
         .finish();
 
     let server = HttpServer::new(move || {
         let rpc = rpc.clone();
-        App::new().service(
-            web::service("/api")
-                .guard(actix_web::guard::Post())
-                .finish(rpc.into_web_service()),
-        )
+        let rpc_get = rpc_get.clone();
+        App::new()
+            .service(
+                web::service("/api")
+                    .guard(actix_web::guard::Post())
+                    .finish(rpc.into_web_service()),
+            )
+            .service(
+                web::service("/read")
+                    .guard(actix_web::guard::Get())
+                    .finish(rpc_get.into_web_service()),
+            )
     })
     .listen(listener)?
     .run();
