b684e1ebd2afe19e2cb72f83fac2c1b873085819
Igor Gilmutdinov <bladoff@gmail.com>
2021-07-09 01:49:37 +0300

    new project for embedding experiments

--- flake.nix
+++ flake.nix
@@ -5,17 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, crate2nix, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     let
       myapp = "poe-system";
       rust-version = "1.51.0";
@@ -30,11 +26,15 @@
             };
             cargo = rustc;
           })
+          (self: super: rec {
+            rustc-nightly =
+              self.rust-bin.nightly."2021-04-22".default.override {
+                extensions = [ "rustc-dev" "llvm-tools-preview" ];
+              };
+          })
         ];
         pkgs = import nixpkgs { inherit system overlays; };
-        crate2nix-pkgs = import crate2nix { inherit pkgs; };
         lib = pkgs.lib;
-        project = import ./Cargo.nix { inherit pkgs; };
 
         buildInputs = with pkgs; [
           sccache
@@ -43,24 +43,16 @@
           mysql
           openssl
           pkg-config
-          nasm
           cmake
           zlib
           gnumake
-          fswatch
           python3
           jq
-          crate2nix-pkgs
           nixos-shell
         ];
         nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig nixpkgs-fmt ];
 
       in rec {
-        packages.${myapp} = project.rootCrate.build;
-        defaultPackage = packages.${myapp};
-        overlay = final: prev: { poe-system = packages.${myapp}; };
-        nixosModule = import ./module.nix;
-
         devShell = with pkgs;
           mkShell {
             name = "rust";
