1bc0b71d38ba1d393017e406c834535291c47697
Igor Gilmutdinov <bladoff@gmail.com>
2023-05-04 23:12:49 +0300

    Working tauri app.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "rust workspace";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=2caf4ef5005ecc68141ecb4aac271079f7371c44";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
@@ -27,12 +27,10 @@
           rust-analyzer
           trunk
 
-          sccache
           sqlite
           postgresql
           mysql
           openssl
-          pkg-config
           cmake
           zlib
           gnumake
@@ -42,8 +40,31 @@
           git
           crate2nix
           vscodium
+          nodejs
+          curl
+
+          wget
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
         ];
-        nativeBuildInputs = with pkgs; [ pkgconfig nixpkgs-fmt ];
+        nativeBuildInputs = with pkgs; [ pkg-config nixpkgs-fmt ];
+        libs = with pkgs; [
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+          vulkan-loader
+          llvmPackages_15.llvm
+        ];
 
       in
       rec {
@@ -54,8 +75,8 @@
             inherit nativeBuildInputs;
 
             shellHook = ''
-              export RUSTC_WRAPPER="${pkgs.sccache}/bin/sccache"
-              export PATH=$PATH:$HOME/.cargo/bin
+              export PATH=$PATH:$HOME/.cargo/bin:$PWD/app/node_modules/.bin
+              export LD_LIBRARY_PATH=${lib.makeLibraryPath libs}
             '';
           };
       });
