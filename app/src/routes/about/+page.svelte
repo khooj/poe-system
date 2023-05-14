764685054949b9b04af452ed5ff88011aa6255af
Igor Gilmutdinov <bladoff@gmail.com>
2023-05-14 18:53:11 +0300

    Removes tauri and leptos projects.

--- app/src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import Greet from '../../lib/Greet.svelte';
-</script>
-
-<h1>Sveltekit</h1>
-<Greet />
-
-<h1>abount</h1>
--- app/src/routes/blog/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Greet from '$lib/Greet.svelte';
-	export let data;
-</script>
-
-<h1>{data.post.title}</h1>
-<div>{@html data.post.content}</div>
-<Greet />
-
