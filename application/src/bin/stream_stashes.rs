ba6c3ee84a66603e53505ce96eb95ad260679f78
Igor Gilmutdinov <bladoff@gmail.com>
2025-01-05 09:09:17 +0300

    feat(app/stream_stashes): async version

--- application/src/bin/stream_stashes.rs
+++ application/src/bin/stream_stashes.rs
@@ -1,32 +1,57 @@
-use compress_tools::{list_archive_files, uncompress_archive_file};
+use compress_tools::tokio_support::{list_archive_files, uncompress_archive_file};
 use core::str;
 use futures::future::ok;
 use std::env::args;
-use std::fs::File;
-use std::io::{BufReader, BufWriter, Cursor, Read, Seek, Write};
+use std::os::unix::fs::MetadataExt;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+
+//use std::fs::File;
+//use std::io::{BufReader, BufWriter, Cursor, Read, Seek, Write};
+use tokio::fs::File;
 
 const NEXT_CHANGE_LINE: &str = "\"next_change_id\"";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+enum Msg {
+    Exit,
+    Data(String),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = args().into_iter().collect::<Vec<String>>();
     if args.len() < 2 {
         eprintln!("Usage: {} <filename>", args[0]);
         return Ok(());
     }
 
-    let mut src = File::open(&args[1])?;
-    let mut archive = Vec::with_capacity(src.metadata().unwrap().len() as usize);
-    src.read_to_end(&mut archive)?;
-    let mut arc_buf = Cursor::new(&archive);
-    let files = list_archive_files(&mut arc_buf)?;
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+    let handle = tokio::spawn(async move {
+        loop {
+            match rx.recv().await {
+                Some(msg) => match msg {
+                    Msg::Exit => return,
+                    Msg::Data(s) => io::stdout()
+                        .write_all(s.as_bytes())
+                        .await
+                        .expect("cannot write to stdout"),
+                },
+                None => {}
+            }
+        }
+    });
+
+    let mut src = File::open(&args[1]).await?;
+    let mut archive = Vec::with_capacity(src.metadata().await?.size() as usize);
+    src.read_to_end(&mut archive).await?;
+    //let mut arc_buf = Cursor::new(&archive);
+    let files = list_archive_files(&mut &archive[..]).await?;
     //println!("{:?}", files);
     let mut buf = Vec::with_capacity(10 * 1024 * 1024);
     let mut q = Some("index.json".to_string());
-    let mut stdout = std::io::stdout();
     while let Some(f) = q.take() {
         buf.clear();
-        arc_buf.seek(std::io::SeekFrom::Start(0))?;
-        let bytes = uncompress_archive_file(&mut arc_buf, &mut buf, &f)?;
+        let bytes = uncompress_archive_file(&mut &archive[..], &mut buf, &f).await?;
         let s = unsafe { str::from_utf8_unchecked(&buf[..bytes]) };
         if let Some(mut idx) = s.find(NEXT_CHANGE_LINE) {
             idx += NEXT_CHANGE_LINE.len();
@@ -38,8 +63,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 q = Some(filename);
             }
         }
-        stdout.write_all(s.as_bytes())?;
+        tx.send(Msg::Data(s.to_string()))?;
     }
 
+    tx.send(Msg::Exit)?;
+    handle.await?;
     Ok(())
 }
