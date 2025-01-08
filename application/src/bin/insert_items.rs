e58b62f5e085515163df6ec9da0aa64d90176b2a
Igor Gilmutdinov <bladoff@gmail.com>
2025-01-08 10:58:16 +0300

    feat(app/insert_items): add task pool for redis inserts

--- application/src/bin/insert_items.rs
+++ application/src/bin/insert_items.rs
@@ -1,9 +1,11 @@
 use std::{collections::HashMap, num::NonZeroUsize, time::Duration};
 
 use application::pipe_stashes::{insert_mods, parse_mods};
+use async_channel::bounded;
 use cassandra_cpp::Cluster;
 use clap::{Parser, Subcommand};
-use public_stash::models::PublicStashData;
+use domain::Mod;
+use public_stash::models::{Item, PublicStashData};
 use redis::AsyncCommands;
 use tokio::io::{self, AsyncBufReadExt, BufReader, Lines, Stdin};
 
@@ -85,7 +87,10 @@ async fn insert_into_cass(task_num: usize, mut lines: Lines<BufReader<Stdin>>) -
     Ok(())
 }
 
-async fn insert_into_redis(mut lines: Lines<BufReader<Stdin>>) -> SharedResult<()> {
+async fn insert_into_redis(
+    task_num: usize,
+    mut lines: Lines<BufReader<Stdin>>,
+) -> SharedResult<()> {
     let cwd = std::env::current_dir()?;
     let client = redis::Client::open(format!(
         "redis+unix:{}",
@@ -95,38 +100,74 @@ async fn insert_into_redis(mut lines: Lines<BufReader<Stdin>>) -> SharedResult<(
             .to_str()
             .unwrap()
     ))?;
-    let mut conn = client.get_multiplexed_async_connection().await?;
 
-    let mut iter: redis::AsyncIter<String> = conn.scan().await?;
-    let mut to_remove = vec![];
-    while let Some(item) = iter.next_item().await {
-        if item.contains("affix") {
-            to_remove.push(item);
-        }
+    let pool = tokio_task_pool::Pool::bounded(task_num)
+        .with_spawn_timeout(Duration::from_secs(5))
+        .with_run_timeout(Duration::from_secs(20));
+
+    let (tx, rx) = bounded(task_num);
+    for _ in 0..task_num {
+        tx.send(client.get_multiplexed_async_connection().await?)
+            .await?;
     }
 
-    std::mem::drop(iter);
-
-    for item in to_remove {
-        let l = conn.llen(&item).await?;
-        conn.lpop(&item, NonZeroUsize::new(l)).await?;
-    }
-
-    while let Ok(Some(line)) = lines.next_line().await {
-        let stash_info: PublicStashData = serde_json::from_str(&line)?;
-        for stash in stash_info.stashes {
-            let mut affixes: HashMap<String, Vec<String>> = HashMap::new();
-            for item in stash.items {
-                let mods = parse_mods(&item);
-                for m in mods {
-                    let lst = affixes.entry(format!("affix:{}", m.stat_id)).or_default();
-                    lst.push(item.id.clone().unwrap());
+    let mut handles = vec![];
+    let rx_cloned = rx.clone();
+    let tx_cloned = tx.clone();
+    let h = pool
+        .spawn(async move {
+            let rx = rx_cloned;
+            let tx = tx_cloned;
+            let mut conn = rx.recv().await.unwrap();
+            let mut iter: redis::AsyncIter<String> = conn.scan().await.unwrap();
+            let mut to_remove = vec![];
+            while let Some(item) = iter.next_item().await {
+                if item.contains("affix") {
+                    to_remove.push(item);
                 }
             }
-            for (k, lst) in affixes {
-                conn.lpush(k, lst).await?;
+            std::mem::drop(iter);
+
+            for item in to_remove {
+                let l = conn.llen(&item).await.unwrap();
+                let _: Vec<String> = conn.lpop(&item, NonZeroUsize::new(l)).await.unwrap();
             }
-        }
+
+            tx.send(conn).await.unwrap();
+        })
+        .await?;
+    h.await??;
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        let rx_cloned = rx.clone();
+        let tx_cloned = tx.clone();
+        let h = pool
+            .spawn(async move {
+                let tx = tx_cloned;
+                let rx = rx_cloned;
+                let mut conn = rx.recv().await.unwrap();
+                let stash_info: PublicStashData = serde_json::from_str(&line).unwrap();
+                let mut affixes: HashMap<String, Vec<&str>> = HashMap::new();
+                for stash in &stash_info.stashes {
+                    for item in &stash.items {
+                        let mods = parse_mods(&item);
+                        for m in mods {
+                            let lst = affixes.entry(format!("affix:{}", m.stat_id)).or_default();
+                            lst.push(item.id.as_ref().unwrap());
+                        }
+                    }
+                }
+                for (k, lst) in affixes {
+                    let _: usize = conn.lpush(k, lst).await.unwrap();
+                }
+                tx.send(conn).await.unwrap();
+            })
+            .await?;
+        handles.push(h);
+    }
+
+    for h in handles {
+        h.await??;
     }
 
     Ok(())
@@ -141,7 +182,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match cli.command {
         Command::Cassandra => insert_into_cass(cli.task_num, lines).await?,
-        Command::Redis => insert_into_redis(lines).await?,
+        Command::Redis => insert_into_redis(cli.task_num, lines).await?,
     };
 
     Ok(())
