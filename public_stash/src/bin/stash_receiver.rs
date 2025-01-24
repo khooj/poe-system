d75082840b39d6b6828eddb75b5ac6df5b9fbf75
Igor Gilmutdinov <bladoff@gmail.com>
2025-01-24 17:03:33 +0300

    feat: restore stash_receiver app and make temporary empty storage crate

--- public_stash/src/bin/stash_receiver.rs
+++ public_stash/src/bin/stash_receiver.rs
@@ -1,10 +1,7 @@
-use crate::infrastructure::repositories::postgres::{
-    raw_item::RawItem, raw_item_repository::RawItemRepository,
-};
-use public_stash::{client::Client, models::Error as StashError};
+use public_stash::client::{Client, Error as StashError};
+use storage::{DynItemRepository, LatestStashId};
 use thiserror::Error;
 use tracing::{error, info, instrument, trace};
-use uuid::Uuid;
 
 #[derive(Error, Debug)]
 pub enum StashReceiverError {
@@ -15,14 +12,14 @@ pub enum StashReceiverError {
 }
 
 pub struct StashReceiver {
-    repository: RawItemRepository,
+    repository: DynItemRepository,
     client: Client,
     only_leagues: Vec<String>,
 }
 
 impl StashReceiver {
     pub fn new(
-        repository: RawItemRepository,
+        repository: DynItemRepository,
         client: Client,
         only_leagues: Vec<String>,
     ) -> StashReceiver {
@@ -37,16 +34,15 @@ impl StashReceiver {
 impl StashReceiver {
     #[instrument(err, skip(self))]
     pub async fn receive(&mut self) -> Result<(), anyhow::Error> {
-        let mut t = self.repository.begin().await?;
-        let res = self.repository.get_stash_id(&mut t).await?;
-        trace!("latest stash id from repo: {}", res);
-        let mut k = self.client.get_latest_stash(Some(&res)).await?;
+        let res = self.repository.get_stash_id().await?;
+        trace!("latest stash id from repo: {}", res.id);
+        let mut k = self.client.get_latest_stash(Some(&res.id)).await?;
         trace!("received stash with next id: {}", k.next_change_id);
         if k.stashes.is_empty() {
             return Ok(());
         }
 
-        if self.only_leagues.len() > 0 {
+        if !self.only_leagues.is_empty() {
             k.stashes.retain(|el| {
                 self.only_leagues
                     .contains(el.league.as_ref().unwrap_or(&String::new()))
@@ -61,17 +57,19 @@ impl StashReceiver {
             let acc = d.account_name.as_ref().unwrap();
             let stash = d.stash.as_ref().unwrap();
 
-            if d.items.len() == 0 {
-                self.repository.delete_item(&mut t, acc, stash).await?;
+            if d.items.is_empty() {
+                self.repository.delete_item(acc, stash).await?;
                 continue;
             }
 
             // doesnt work
-            let items = d.items.into_iter().map(|m| {
-                let id = Uuid::new_v4().to_string();
-                RawItem::new(&id, acc, stash, m)
-            }).collect();
-            self.repository.insert_items(&mut t, items).await?;
+            // let items = d
+            //     .items
+            //     .into_iter()
+            //     .filter_map(|m| Item::try_from(m).ok())
+            //     .collect();
+            // TODO: logic for filtering items
+            self.repository.insert_items(vec![]).await?;
             // for item in d.items {
             //     // we cant be sure that every item have unique id
             //     // so we generate it themselves
@@ -82,21 +80,13 @@ impl StashReceiver {
             // }
         }
         self.repository
-            .set_stash_id(&mut t, &k.next_change_id)
+            .set_stash_id(LatestStashId {
+                id: k.next_change_id.clone(),
+            })
             .await?;
-        t.commit().await?;
         info!(id = %k.next_change_id, "successfully received and inserted");
         Ok(())
     }
-
-    #[instrument(err, skip(self))]
-    pub async fn ensure_stash_id(&mut self, id: String) -> Result<(), anyhow::Error> {
-        let mut t = self.repository.begin().await?;
-        let r = self.repository.get_stash_id(&mut t).await;
-        if r.is_err() {
-            self.repository.set_stash_id(&mut t, &id).await?;
-        }
-        t.commit().await?;
-        Ok(())
-    }
 }
+
+fn main() {}
